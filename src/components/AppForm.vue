1f10fc274b94db5f5032685880de1c3e832e2694
robin-sevelin <robinsevelin@hotmail.com>
2023-06-01 21:00:28 +0200

    refactor form

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -2,55 +2,41 @@
   import { ref } from 'vue';
   import { Player } from '../Models/Player';
 
-  const playerNameX = ref('');
-  const playerNameO = ref('');
+  const playerX = ref('');
+  const playerO = ref('');
 
-  const playerX = ref<Player>(new Player('', '✗', 0, 0, false));
-  const playerO = ref<Player>(new Player('', '⭕️', 1, 0, false));
-
-  const players = ref<Player[]>([]);
+  const players = ref<Player[]>([
+    new Player('', '✗', 0, 0, false),
+    new Player('', '⭕️', 1, 0, false),
+  ]);
 
   const emits = defineEmits(['players']);
 
-  const submitUserX = () => {
-    playerX.value.name = playerNameX.value;
-    playerX.value.isCreated = true;
-    players.value.push(playerX.value);
-  };
-
-  const submitUserO = () => {
-    playerO.value.name = playerNameO.value;
-    playerO.value.isCreated = true;
-    players.value.push(playerO.value);
-
-    emits('players', players.value);
+  const submitUser = () => {
+    if (players.value[0].isCreated === false) {
+      players.value[0].name = playerX.value;
+      players.value[0].isCreated = true;
+    } else {
+      players.value[1].name = playerO.value;
+      players.value[1].isCreated = true;
+      emits('players', players.value);
+    }
   };
 </script>
 
 <template>
-  <form
-    v-if="playerX.isCreated === false"
-    @submit.prevent="submitUserX"
-    class="form-container"
-  >
-    <div class="input-container">
+  <form @submit.prevent="submitUser" class="form-container">
+    <div class="input-container" v-if="players[0].isCreated === false">
       <label for="inputX">Name for player ✗</label>
-      <input id="inputX" type="text" v-model="playerNameX" autofocus />
+      <input id="inputX" type="text" v-model="playerX" autofocus />
     </div>
-    <div class="button-container">
-      <button :disabled="playerNameX === ''">Done</button>
-    </div>
-  </form>
-
-  <form v-else class="form-container" @submit.prevent="submitUserO">
-    <div class="input-container">
+    <div class="input-container" v-else>
       <label for="inputO">Name for player ⭕️</label>
-      <input id="inputO" type="text" v-model="playerNameO" autofocus />
+      <input id="inputO" type="text" v-model="playerO" autofocus />
     </div>
-
-    <div class="button-container">
-      <button :disabled="playerNameO === ''">Done</button>
-    </div>
+    <button :disabled="!players[0].isCreated ? playerX === '' : playerO === ''">
+      Done
+    </button>
   </form>
 </template>
 
